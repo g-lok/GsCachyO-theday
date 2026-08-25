42185f3941cb335cf6afd6ebabd1668a4180eaef
G <g@devnull.rip>
2026-08-24 19:45:42 -0700

    added quick push+squash merge script

--- files/dotfiles/bin/bin/pushnmerge.sh
+++ files/dotfiles/bin/bin/pushnmerge.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env zsh
 
-setopt BANG_HIST
 BOOKMARK=$1
 
 jj git push -b "$BOOKMARK"
 DESCRIPTION=$(jj show | sed -n '7p' | xargs)
 gh pr create -B main -H "$BOOKMARK" -t "$DESCRIPTION" -b "" | xargs -I {} gh pr merge "{}" -s -d
+git push origin --delete "$BOOKMARK"
 jj git fetch && jj new main
